7eefd21060d21b1e3b6c8860d1914467641b499c
nitin887 <nitinrao9213@gmail.com>
2026-01-12 21:46:57 +0530

    message

--- Test.cs
+++ Test.cs
@@ -1,42 +1,15 @@
-using System.Linq.Expressions;
+using System;
+using System.Collections.Generic;
 
 class Test
 {
-    static void Main(string[] args)
-    {
-        /*
-        n1 n2
-        1   2
-        n1=n1+n2=3
-        n2=n1-n2=1
-        n1=n1-n2=2
-        
-    */
-    int number=153;
-    int temp=number;
-    int sum=0;
-    int remainder;
-        while (temp != 0)
-        {
-            remainder=temp%10;
-            sum=sum+remainder*remainder*remainder;
-            temp=temp/10;
+	static void Main()
+	{
+		List<int> list = new List<int>();
 
-            
-        }
-        if(number==sum)
-        {
-            Console.WriteLine("no is palindrome");
-        }
-        else
-        {
-            Console.WriteLine("not an palindrome");
-        }
-
-     
-         
-
-
-    }
-} 
-
+		if (list.Count > 0)
+			Console.WriteLine(list[0]);
+		else
+			Console.WriteLine("List is empty");
+	}
+}
