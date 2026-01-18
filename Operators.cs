47f77bcb38b7324898681492326f7f101de1c74a
nitin887 <nitinrao9213@gmail.com>
2026-01-18 22:25:29 +0530

    message

--- Operators.cs
+++ Operators.cs
@@ -1,14 +1,19 @@
 /*
-Use bitwise operators to check if a number is even.
+Implement multiplication using only addition.
 */
+using System;
+
 class Operator
 {
     static void Main(string[] args)
     {
-      
-
-
-
-        
+     int a=5;
+     int b=10;
+     int result=0;
+     for(int i = 1; i <=b ;i++)
+     {
+       result=a+result;  
+    }
+    Console.WriteLine("Multiplication of 5 using addition is: " + result);
     }
 }
